d3342cf723b95a42923f368a9f660d9da86a25ed
Roman <36523168+romanazimov@users.noreply.github.com>
2023-07-09 13:20:16 -0400

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,68 +1,11 @@
-// import logo from './logo.svg';
+import React from "react"
 import './App.css';
-// import MyButton from './Components/MyButton'
 import Nav from './Components/nav/Nav'
 import Bio from './Components/bio/Bio'
 import Projects from './Components/projects/Projects'
-// import Table from './Components/table/Table'
-import React from "react"
-// import logo from './images/pngwing.com.png'
-
-// function MyButton() {
-//   return (
-//     <button>
-//       I'm a button
-//     </button>
-//   )
-// }
-
-
-// function App() {
-//   return (
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <a
-//     //       className="App-link"
-//     //       href="https://reactjs.org"
-//     //       target="_blank"
-//     //       rel="noopener noreferrer"
-//     //     >
-//     //       Learn React
-//     //     </a>
-//     //   </header>
-//     // </div>
-//     <div>
-//       Hello World!!!12312
-//       <MyButton message={'Hello World!'}/>
-//     </div>
-//   );
-// }
 
 class App extends React.Component {
   render() {
-    const characters = [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
-
     return (
       <body>
         <Nav />
