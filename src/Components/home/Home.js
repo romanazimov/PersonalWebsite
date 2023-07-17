3c13706f8d9fc2f344daee3db7a2a4b711a1bdbb
Roman <36523168+romanazimov@users.noreply.github.com>
2023-07-16 21:39:56 -0400

    Modified home section with updated text

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,10 +1,18 @@
-import Bio from '../bio/Bio';
 import "./Home.css"
+import logo from "../../images/avatar_roman.png";
+import hand from "../../images/waving_hand.png";
 
 function Home() {
     return (
         <section id="home">
-            <Bio />
+            <div id="cont-bio">
+                <div id="bio-info">
+                    <h1>Hello, my name is Roman <img id="hand" src={hand} alt={"hand"} /></h1>
+                    <p className="short-bio">I'm a full-stack software developer based out of New Jersey with a passion 
+                    for creating innovative solutions and crafting seamless user experiences.</p>
+                </div>
+                <img id="avatar" src={logo} alt={"logo"} />
+            </div>
         </section>
     );
 }
