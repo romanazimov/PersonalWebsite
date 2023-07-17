25bb86a0b1b7448f6bd1f08a0f8c4993d08c5c31
Roman <36523168+romanazimov@users.noreply.github.com>
2023-07-17 13:49:50 -0400

    About section modified with img, title, and desc.

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -1,7 +1,22 @@
+import './About.css'
+import coffee_work from '../../images/cofee_work.png'
+
 function About() {
     return (
         <section id="about">
-            <h1>Hello from About!</h1>
+            <div className="cont-about">
+                <img className="img-about" src={coffee_work} alt={"logo"} />
+                <div>
+                    <p className='title'>ABOUT ME</p>
+                    <p className='description'>As a full-stack developer, I bring extensive professional experience in back-end 
+                        development along with front-end expertise in working with large datasets.  With 
+                        a knack for design, I can transform ideas into engaging user experiences and handle 
+                        end-to-end deployment and hosting configurations. Beyond my professional work, I'm
+                        an avid front-end enthusiast, pursuing it as a hobby and showcasing my skills through
+                        this personal website. When I'm not coding, you can find me exploring the great
+                        outdoors, hiking, running, or enjoying my time out on the water.</p>
+                </div>
+            </div>
         </section>
     );
 }
--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -8,7 +8,7 @@ function Home() {
             <div id="cont-bio">
                 <div id="bio-info">
                     <h1>Hello, my name is Roman <img id="hand" src={hand} alt={"hand"} /></h1>
-                    <p className="short-bio">I'm a full-stack software developer based out of New Jersey with a passion 
+                    <p id="short-bio">I'm a full-stack software developer based out of New Jersey with a passion 
                     for creating innovative solutions and crafting seamless user experiences.</p>
                 </div>
                 <img id="avatar" src={logo} alt={"logo"} />
