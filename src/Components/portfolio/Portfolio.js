0a5c225dc91834a8c1f69434a469de0162089d1e
Roman <36523168+romanazimov@users.noreply.github.com>
2023-07-18 19:11:36 -0400

    Revamped the portfolio with a project in the works

---
+++ src/Components/portfolio/Portfolio.js
@@ -0,0 +1,38 @@
+import './Portfolio.scss';
+import gym from '../../images/proj_gym.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faHtml5, faCss3Alt, faJs, faUpRightFromSquare } from '@fortawesome/free-brands-svg-icons';
+
+function Portfolio() {
+    return(
+        <section id='portfolio'>
+            <div className='container'>
+                <p className='title'>PORTFOLIO</p>
+                <div className='project-cont'>
+                    <img className="image" src={gym} alt={""} />
+                    <div className='right-section'>
+                        <p className='proj-title'>Gym Landing Page</p>
+                        <p className='proj-desc'>
+                            A simple yet effective single page application showcasing
+                            the gym, memberships, trial and personal trainers that it
+                            has to offer.
+                        </p>
+                        <div className='proj-tech'>
+                            {/* React, HTML, CSS, Javascript */}
+                            <FontAwesomeIcon className="icon-react" icon={faReact} />
+                            <FontAwesomeIcon className="icon-html" icon={faHtml5} />
+                            <FontAwesomeIcon className="icon-css" icon={faCss3Alt} />
+                            <FontAwesomeIcon className="icon-javascript" icon={faJs} />
+                        </div>
+                        <div className='proj-links'>
+                            <a href="https://github.com/romanazimov" rel="noreferrer" target="_blank"><button className='btn-code'>Code</button></a>
+                            <a href="https://github.com/romanazimov" rel="noreferrer" target="_blank"><button className='btn-demo'>Demo</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Portfolio;
