6d990bb30ef7c9c9a9da34a2e8295a797fa916ad
Roman <36523168+romanazimov@users.noreply.github.com>
2023-07-19 11:48:51 -0400

    Contact section updated w/ email, linkedin, github

--- src/Components/contact/Contact.js
+++ src/Components/contact/Contact.js
@@ -1,19 +1,40 @@
 import './Contact.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelopeOpenText, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Contact() {
     return (
         <section id="contact">
             <div className='container'>
                 <p className='title'>CONTACT</p>
-                <div className='content'>
-                    <div className='icon-container'>
-                        <FontAwesomeIcon className="icon-email" icon={faEnvelopeOpenText} />
+                <div className='content-container'>
+                    <div className='email-content'>
+                        <div className='icon-container'>
+                            <FontAwesomeIcon className="icon-email" icon={faEnvelopeOpenText} />
+                        </div>
+                        <div>
+                            <p className='primary-text'>Email</p>
+                            <a href="mailto:azimov308@live.com"><p className="secondary-text">azimov308@live.com</p></a>
+                        </div>
                     </div>
-                    <div className='email-info'>
-                        <p className='primary-text'>Email</p>
-                        <a className="secondary-text" href="mailto:azimov308@live.com">azimov308@live.com</a>
+                    <div className='linkedin-content'>
+                        <div className='icon-container'>
+                            <FontAwesomeIcon className="icon-linkedin" icon={faLinkedinIn} />
+                        </div>
+                        <div>
+                            <p className='primary-text'>LinkedIn</p>
+                            <a href="https://www.linkedin.com/in/roman-azimov/" rel="noreferrer" target="_blank"><p className="secondary-text">/in/roman-azimov</p></a>
+                        </div>
+                    </div>
+                    <div className='github-content'>
+                        <div className='icon-container'>
+                        <FontAwesomeIcon className="icon-github" icon={faGithub} />
+                        </div>
+                        <div>
+                            <p className='primary-text'>GitHub</p>
+                            <a href="https://www.github.com/romanazimov/" rel="noreferrer" target="_blank"><p className="secondary-text">/romanazimov</p></a>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -4,7 +4,7 @@ import hand from "../../images/waving_hand.png";
 import resume from "../../files/razimov_resume.docx";
 // import linkedin_logo from "../../images/linkedin_logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+// import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Home() {
     return (
@@ -17,7 +17,7 @@ function Home() {
                         for creating innovative solutions and crafting seamless user experiences.</p>
                         <div className="bio-actions">
                             <a href={resume} download="roman_azimov_resume"><button className="resume-download">Download Resume</button></a>
-                            <a href="https://www.linkedin.com/in/roman-azimov/" rel="noreferrer" target="_blank"><FontAwesomeIcon className="linkedin" icon={faLinkedin} /></a>
+                            {/* <a href="https://www.linkedin.com/in/roman-azimov/" rel="noreferrer" target="_blank"><FontAwesomeIcon className="linkedin" icon={faLinkedin} /></a> */}
                             {/* <img className="linkedin" src={linkedin} alt={"logo"} /> */}
                             {/* <FontAwesomeIcon icon="fa-brands fa-linkedin" /> */}
                         </div>
