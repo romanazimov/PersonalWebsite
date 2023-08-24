5d04c5bafedb2b10a64ee0d12aee82cdba8105ca
Roman <36523168+romanazimov@users.noreply.github.com>
2023-08-24 13:16:02 -0400

    Redid nav entirely to make it much cleaner

--- src/Components/nav/Nav.js
+++ src/Components/nav/Nav.js
@@ -1,104 +1,40 @@
 import './Nav.scss';
-import NavLinks from './NavLinks'
-import { useState, useEffect} from 'react';
-import { Link, useMatch, useResolvedPath, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { Link } from 'react-scroll'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 export default function Nav() {
-	const [isNavOpen, setIsNavOpen] = useState(false);
-	const [linkClicked, setLinkClicked] = useState(false);
-    const navigate = useNavigate();
-
-	useEffect(() => {
-		if (linkClicked) {
-			setIsNavOpen(false);
-			setLinkClicked(false);
-		}
-	}, [isNavOpen, linkClicked]);
-
-    const scrollToSection = (id, path) => {
-        const element = document.getElementById(id);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
-            navigate(path);
-			setLinkClicked(true);
-        }
-    };
-
-    function CustomLink({ to, children, ...props }) {
-        const resolvedPath = useResolvedPath(to);
-        const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-
-        return (
-            <Link className={isActive ? " active" : ""} to={to} {...props}>
-                {children}
-            </Link>
-        );
-    }
+	const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMenu = () => setClick(false)
 
     return (
         <nav>
-            <span className="home-logo">Roman Azimov</span>
-            <div className="routes">
-				<div className='desktop-view'>
-					<ul>
-						<li>
-							<CustomLink to="/" onClick={() => scrollToSection('home', '/')}>
-								Home
-							</CustomLink>
-						</li>
-						<li>
-							<CustomLink to="/about" onClick={() => scrollToSection('about', '/about')}>
-								About
-							</CustomLink>
-						</li>
-						<li>
-							<CustomLink to="/portfolio" onClick={() => scrollToSection('portfolio', '/portfolio')}>
-								Portfolio
-							</CustomLink>
-						</li>
-						<li>
-							<CustomLink to="/contact" onClick={() => scrollToSection('contact', '/contact')}>
-								Contact
-							</CustomLink>
-						</li>
-					</ul>
-				</div>
-                <FontAwesomeIcon className="hamburger-icon" onClick={() => setIsNavOpen(!isNavOpen)} icon={faBars} />
-				<div className={`cont-side-nav ${isNavOpen ? "nav-open" : "nav-closed"}`}>
-					<div className='sidebar'>
-						{/* <div className='cont-exit'>
-							<FontAwesomeIcon className='sidebar-exit' onClick={() => setIsNavOpen(false)} icon={faXmark} />
-						</div> */}
-						<FontAwesomeIcon className='sidebar-exit' onClick={() => setIsNavOpen(false)} icon={faXmark} />
-						<div className='routes'>
-							<ul>
-								<li>
-									<CustomLink to="/" onClick={() => scrollToSection('home', '/')}>
-										Home
-									</CustomLink>
-								</li>
-								<li>
-									<CustomLink to="/about" onClick={() => scrollToSection('about', '/about')}>
-										About
-									</CustomLink>
-								</li>
-								<li>
-									<CustomLink to="/portfolio" onClick={() => scrollToSection('portfolio', '/portfolio')}>
-										Portfolio
-									</CustomLink>
-								</li>
-								<li>
-									<CustomLink to="/contact" onClick={() => scrollToSection('contact', '/contact')}>
-										Contact
-									</CustomLink>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
+            <div className='nav-container'>
+				<span className="home-logo">Roman Azimov</span>
+
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes className='hamburger' />)
+                        : (<FaBars className='hamburger' />)}
+                </div>
+
+                <div className={click ? "nav-menu active" : "nav-menu"}>
+                    <ul>
+                        <li>
+                            <Link to="home" spy={true} smooth={true} offset={-40} duration={500} onClick={closeMenu}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to="about" spy={true} smooth={true} offset={-40} duration={500} onClick={closeMenu}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="portfolio" spy={true} smooth={true} offset={-40} duration={500} onClick={closeMenu}>Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link to="contact" spy={true} smooth={true} offset={-40} duration={500} onClick={closeMenu}>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
-    );
+    )
 }
