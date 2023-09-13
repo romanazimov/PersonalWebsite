cc51222e76103bdeae0862388dc4cfbd4174e954
Roman <36523168+romanazimov@users.noreply.github.com>
2023-09-13 14:11:45 -0400

    Added project in progress, cleaned and updated css

--- src/Components/portfolio/Portfolio.js
+++ src/Components/portfolio/Portfolio.js
@@ -1,6 +1,7 @@
 import './Portfolio.scss';
 import gymImage from '../../images/proj_gym.png'
-import dotnetImage from '../../images/dotnet-core.jpeg'
+import swaggerApiImage from '../../images/swagger_api.png'
+import ecomCandleImage from '../../images/ecom_candle.png'
 import Project from './Project';
 
 function Portfolio() {
@@ -10,20 +11,32 @@ function Portfolio() {
                 <p className='title'>PORTFOLIO</p>
                 <div className='project-cont'>
                     <Project
-                        image={gymImage}
-                        primaryText="GYM24"
-                        secondaryText="A simple yet effective single page application showcasing the gym, perks, memberships, and team that it has to offer."
-                        technologies={['React', 'TypeScript']}
-                        githubLink="https://github.com/romanazimov/gym-app"
-                        demoLink="https://gym.romanazimov.com/"
+                        image={ecomCandleImage}
+                        primaryText="[In Progress] Ecommerce Store"
+                        secondaryText="Utilizing a headless architecture, I am creating an ecommerce store. 
+                        Redux will be used for state management and data fetching. Stripe will also be used 
+                        for purchases."
+                        technologies={['React+Vite', 'Redux', 'Stripe']}
+                        githubLink="https://github.com/romanazimov/candle-app"
+                        demoLink="https://candle.romanazimov.com/"
                     />
                     <Project
-                        image={dotnetImage}
+                        image={swaggerApiImage}
                         primaryText="Ecommerce Candle API"
-                        secondaryText="The backend api of an ecommerce app to handle users, purchase orders, authentication (JWT) and authorization."
+                        secondaryText="The back-end api of an ecommerce app to handle users, purchase orders, 
+                        authentication (JWT) and authorization."
                         technologies={['.NET Core', 'Entity FWK', 'PostgreSQL']}
                         githubLink="https://github.com/romanazimov/CandleApi"
                         demoLink=""
+                    />
+                    <Project
+                        image={gymImage}
+                        primaryText="GYM24"
+                        secondaryText="A simple yet effective single page application showcasing the gym, perks, 
+                        memberships, and team that it has to offer."
+                        technologies={['React', 'TypeScript']}
+                        githubLink="https://github.com/romanazimov/gym-app"
+                        demoLink="https://gym.romanazimov.com/"
                     />
                 </div>
             </div>
