666e786f27940c21bc3e43ff18b89101bdaef47b
Roman <36523168+romanazimov@users.noreply.github.com>
2023-09-15 12:15:08 -0400

    Removed resume

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,7 +1,6 @@
 import "./Home.scss"
 import avatar from "../../images/avatar_roman.png";
 import hand from "../../images/waving_hand.png";
-import resume from "../../files/razimov_resume.docx";
 
 function Home() {
     return (
@@ -12,9 +11,6 @@ function Home() {
                         <p className="greeting">Hello, my name is Roman <img className="hand" src={hand} alt={""} /></p>
                         <p className="secondary-text">I'm a full-stack software developer based out of New Jersey with a passion 
                         for creating innovative solutions and crafting seamless user experiences.</p>
-                        {/* <div className="bio-actions">
-                            <a href={resume} download="roman_azimov_resume"><button className="resume-download">Download Resume</button></a>
-                        </div> */}
                     </div>
                     <img className="avatar" src={avatar} alt={"logo"} />
                 </div>
